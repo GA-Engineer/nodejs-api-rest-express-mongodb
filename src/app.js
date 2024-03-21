24b7e07d04b43adaeb322a988dab3772e8b1f1c5
Gabriella Andrade <gabriellaandrade.d@hotmail.com>
2024-03-20 23:10:16 -0300

    feat: Evoluindo a API

--- src/app.js
+++ src/app.js
@@ -1,52 +1,12 @@
 import express from "express";
 import db from "./config/dbConnect.js"
-import livros from "./models/Livro.js"
+import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexão'));
 db.once("open", () => console.log("Conectado ao banco"));
 
 const app = express();
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node');
-});
-
-app.get('/livros', async (req, res) => {
-    try {
-        const livrosResultado = await livros.find();
-        res.status(200).json(livrosResultado)
-    } catch (err) {
-        res.status(500).json(err);
-    }
-})
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro cadastrado com sucesso');
-});
-
-app.put('/livros/:id', (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.json(livros);
-});
-
-app.get('/livros/:id', (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.json(livros[index]);
-});
-
-app.delete('/livros/:id', (req, res) => {
-    const {id} = req.params;
-    const index = buscaLivro(id);
-    livros.splice(index, 1);
-    res.send(`Livro ${id} removido com sucesso!`);
-});
-
-
-function buscaLivro(id){
-    return livros.findIndex(livro => id == livro.id);
-}
+routes(app);
 
 export default app;
---
+++ src/controllers/livroController.js
@@ -0,0 +1,53 @@
+import livros from "../models/Livro.js";
+
+export default class LivroController {
+
+    static listarLivros = async (req, res) => {
+        try {
+            const livrosResultado = await livros.find();
+            res.status(200).json(livrosResultado)
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+
+    static listarLivroPorId = async (req, res) => {
+        try {
+            const id = req.params.id;
+            const livrosResultado = await livros.findById(id);
+            res.status(200).json(livrosResultado)
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    }
+
+    static cadastrarLivro = async (req, res) => {
+        try {
+            let livro = new livros(req.body);
+            const response = await livro.save();
+            res.status(201).json(response.toJSON())
+        } catch (err) {
+            res.status(500).send({ message: `${err.message} - falha ao cadastrar livro.`})
+        }
+    }
+
+    static atualizarLivro = async (req, res) => {
+        try {
+            const id = req.params.id
+            await livros.findByIdAndUpdate(id, {$set: req.body});
+            res.status(200).send({message: "Livro atualizado com sucesso!"});
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+
+    static excluirLivro = async (req, res) => {
+        try {
+            const id = req.params.id
+            await livros.findByIdAndDelete(id, {$set: req.body});
+            res.status(200).send({message: "Livro excluído com sucesso!"});
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+}
